ca74d5538e6e399c81a4d76fbf4ee4d645c2e530
m <m@amis-mbp.home>
2020-12-16 17:39:42 -0500

    fix

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const options = {
 };
 const io = require("socket.io")(http, options);
 
-app.use("/", express.static("sketch"));
-app.use("/play", express.static("buttons"));
+app.use("/play", express.static("sketch"));
+app.use("/", express.static("buttons"));
 
 const listener = http.listen(process.env.PORT || 3000, process.env.IP, () =>
   console.log("listening on *:3000")
